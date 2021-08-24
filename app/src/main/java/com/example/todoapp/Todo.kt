a1814125d0499df47506a574cd5b331f66b86b17
Kate Bryan <kate.bryan@and.digital>
2021-08-24 18:14:14 +0100

    feat: added toggleIsChecked function

--- app/src/main/java/com/example/todoapp/Todo.kt
+++ app/src/main/java/com/example/todoapp/Todo.kt
@@ -3,5 +3,5 @@ package com.example.todoapp
 // it has a class type of 'data' because all this is for is holding data
 data class Todo (
     val title: String,
-    val isChecked: Boolean = false
+    var isChecked: Boolean = false
 )
--- app/src/main/java/com/example/todoapp/TodoAdapter.kt
+++ app/src/main/java/com/example/todoapp/TodoAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.todoapp
 
+import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import kotlinx.android.synthetic.main.item_todo.view.*
 import androidx.recyclerview.widget.RecyclerView
 
@@ -25,16 +27,29 @@ class TodoAdapter(
         )
     }
 
+    // toggle checked view state
+    private fun toggleStrikeThrough(tvTodoTitle: TextView, isChecked: Boolean) {
+        if (isChecked) {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags or STRIKE_THRU_TEXT_FLAG
+        } else {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+        }
+    }
+
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         // called when a new todo item is visible
         // defines which text you want to text to this view
         val currentTodo = todos[position]
 
         holder.itemView.apply {
-            tvTodoTitle
-            cbDone
+            tvTodoTitle.text = currentTodo.title
+            cbDone.isChecked = currentTodo.isChecked
+            toggleStrikeThrough(tvTodoTitle, currentTodo.isChecked)
+            cbDone.setOnCheckedChangeListener { _, isChecked ->
+                toggleStrikeThrough(tvTodoTitle, isChecked)
+                currentTodo.isChecked = !currentTodo.isChecked
+            }
         }
-        currentTodo.isChecked
     }
 
     override fun getItemCount(): Int {
