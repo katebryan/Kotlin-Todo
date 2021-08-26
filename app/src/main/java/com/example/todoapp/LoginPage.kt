8b825fb1ec5340b6b25248a828ac8f8c6fa57d52
Kate Bryan <kate.bryan@and.digital>
2021-08-26 13:55:16 +0100

    feat: added a login page with button click functionality

--- app/src/main/java/com/example/todoapp/LoginPage.kt
+++ app/src/main/java/com/example/todoapp/LoginPage.kt
@@ -1,4 +1,31 @@
 package com.example.todoapp
 
-class LoginPage {
+import android.os.Bundle
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LoginPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page)
+
+        // get reference to all views
+        val etUsername = findViewById<EditText>(R.id.et_user_name)
+        val etPassword = findViewById<EditText>(R.id.et_password)
+        val btnSubmit = findViewById<Button>(R.id.btn_submit)
+
+        // set on-click listener
+        btnSubmit.setOnClickListener {
+            val username = etUsername.text
+            val password = etPassword.text
+            Toast.makeText(this@LoginPage, username, Toast.LENGTH_LONG).show()
+            println(password)
+
+            // your code to validate the user_name and password combination
+            // and verify the same
+
+        }
+    }
 }
