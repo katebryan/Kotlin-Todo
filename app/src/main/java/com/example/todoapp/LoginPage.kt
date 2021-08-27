16cc09f4d4e5f3f2353d52ed9ad5ef15e55cb5e5
Kate Bryan <kate.bryan@and.digital>
2021-08-27 15:05:54 +0100

    feat: added button config for username, email, password linked to amplify

--- app/src/main/java/com/example/todoapp/LoginPage.kt
+++ app/src/main/java/com/example/todoapp/LoginPage.kt
@@ -6,7 +6,9 @@ import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.amplifyframework.AmplifyException
+import com.amplifyframework.auth.AuthUserAttributeKey
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
+import com.amplifyframework.auth.options.AuthSignUpOptions
 import com.amplifyframework.core.Amplify
 
 
@@ -28,13 +30,27 @@ class LoginPage : AppCompatActivity() {
             Log.e("MyAmplifyApp", "Could not initialize Amplify", error)
         }
 
+//        Amplify.Auth.confirmSignUp(
+//            "username", "0000",
+//            { result ->
+//                if (result.isSignUpComplete) {
+//                    Log.i("AuthQuickstart", "Confirm signUp succeeded")
+//                } else {
+//                    Log.i("AuthQuickstart","Confirm sign up not complete")
+//                }
+//            },
+//            { Log.e("AuthQuickstart", "Failed to confirm sign up", it) }
+//        )
+
         // get reference to all views
-        val etUsername = findViewById<EditText>(R.id.et_user_name)
+        val etEmail = findViewById<EditText>(R.id.et_email)
+        val etUsername = findViewById<EditText>(R.id.et_username)
         val etPassword = findViewById<EditText>(R.id.et_password)
         val btnSubmit = findViewById<Button>(R.id.btn_submit)
 
         // set on-click listener
         btnSubmit.setOnClickListener {
+            val email = etEmail.text
             val username = etUsername.text
             val password = etPassword.text
 
@@ -42,10 +58,18 @@ class LoginPage : AppCompatActivity() {
 
             println(password)
 
-            if (username.length > 5) {
-                Toast.makeText(this@LoginPage, username, Toast.LENGTH_LONG).show()
+            if (email.length > 5) {
+                Toast.makeText(this@LoginPage, email, Toast.LENGTH_LONG).show()
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
+                val options = AuthSignUpOptions.builder()
+                    .userAttribute(AuthUserAttributeKey.email(), email.toString())
+                    .build()
+                Amplify.Auth.signUp(username.toString(), password.toString(), options,
+                    { Log.i("AuthQuickStart", "Sign up succeeded: $it") },
+                    { Log.e ("AuthQuickStart", "Sign up failed", it) }
+                )
+
             } else {
                 Toast.makeText(this@LoginPage, errorText, Toast.LENGTH_LONG).show()
 
